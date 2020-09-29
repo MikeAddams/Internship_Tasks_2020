d02f15ce7aab469de7a8707badd2294cb5304bef
Florin <unknown.name.0@Mail.ru>
2020-09-29 11:58:09 +0300

    added last condition and waiting key at the end

--- Flow_control/Flow_control/Program.cs
+++ Flow_control/Flow_control/Program.cs
@@ -13,24 +13,27 @@ namespace Flow_control
 
             if( !string.IsNullOrEmpty(str) )
             {
-                if( str.StartsWith("A") || str.StartsWith("c") )
+                if (str.StartsWith("A") || str.StartsWith("c"))
                 {
                     str = str.Remove(0, 1);
 
                     Console.WriteLine("\nFirst character was removed\nCurrent string => " + str);
                 }
-                else if( Char.IsNumber(str, 0) && !Char.IsNumber(str, 1) )
+                else if (Char.IsNumber(str, 0) && !Char.IsNumber(str, 1))
                 {
-                    int n = Int16.Parse( str.Substring(0, 1) );
+                    int n = Int16.Parse(str.Substring(0, 1));
 
-                    if( n < 5 )
+                    if (n < 5)
                         Console.WriteLine("First char is a number lower than 5");
                 }
+                else
+                    Console.WriteLine("The input does not meet the requirements.");
 
             }
             else
                 Console.WriteLine("empty input");
 
+            Console.ReadKey();
         }
     }
 }
