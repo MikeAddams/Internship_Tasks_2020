b4bb46785a590416b8eba4e16aac42f05ca3db58
Florin <unknown.name.0@Mail.ru>
2020-09-29 10:47:49 +0300

    added first condition

--- Flow_control/Flow_control/Program.cs
+++ Flow_control/Flow_control/Program.cs
@@ -6,8 +6,23 @@ namespace Flow_control
     {
         static void Main(string[] args)
         {
-            
-            
+            string str;
+
+            Console.WriteLine("Your alphanumerical input: ");
+            str = Console.ReadLine();
+
+            if( !string.IsNullOrEmpty(str) )
+            {
+                if( str.StartsWith("A") || str.StartsWith("c") )
+                {
+                    str = str.Remove(0, 1);
+
+                    Console.WriteLine("\nFirst character was removed\nCurrent string => " + str);
+                }
+
+            }
+            else
+                Console.WriteLine("empty input");
 
         }
     }
