fe3c64f5cfb14d5e272e69a794be85ab214a8454
Florin <unknown.name.0@Mail.ru>
2020-10-05 17:05:31 +0300

    checks inputs and if needed, throws a special exception

---
+++ Exception_Handling/task2/ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp2.SpecialExceptions;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1,
+                   input2;
+
+            Console.WriteLine("Input#1: ");
+            input1 = Console.ReadLine();
+
+            Console.WriteLine("\nInput#2: ");
+            input2 = Console.ReadLine();
+
+            try
+            {
+                validateInput(input1, input2);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void validateInput(string input1, string input2)
+        {
+            if (!Regex.IsMatch(input1, @"^[0-9]+$") || !Regex.IsMatch(input2, @"^[0-9]+$"))
+            {
+                int.TryParse(input1, out int n1);
+                int.TryParse(input1, out int n2);
+
+                if (n1 < 1 || n2 < 1)
+                {
+                    throw new NotPositiveNumberException("One of the inputs is not a number or positive");
+                }
+            }
+        }
+    }
+}
---
+++ Exception_Handling/task2/ConsoleApp2/SpecialExceptions/NotPositiveNumberException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.SpecialExceptions
+{
+    class NotPositiveNumberException : Exception
+    {
+        public override string Message { get; }
+        public NotPositiveNumberException(string message)
+            :base(message)
+        {
+            Message = "\n" + message;
+        }
+    }
+}
